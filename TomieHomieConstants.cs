984872473a2c3a0e903117c3fd1f56328a3fba0f
borosenthusiast <orionhsu552@gmail.com>
2023-04-29 08:33:36 -0700

    quick fix. (#3)

--- TomieHomieConstants.cs
+++ TomieHomieConstants.cs
@@ -9,7 +9,7 @@
         public const string PYFAIL = "Failed to run python script or produce output.";
         public const string ARGFAIL = "Enter a number.";
         public const string PYOUTPUTFAIL = "Failed to produce an output from PY script.";
-        public const string PATH = @"\irregular_tome_grind_solver.exe";
+        public const string PATH = @"\PythonExecutable\irregular_tome_grind_solver.exe";
         public const string CALC = "Calculating! Please wait a moment...";
     }
 }
